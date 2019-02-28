a7a87202d896b0011f9096ff075bf9a98298bff4
Britt Carter <brittbmh@gmail.com>
2019-02-28 16:12:27 -0600

    add delete alert and conditional render for ready to transfer

--- src/components/KoalaList/KoalaTableDetail.js
+++ src/components/KoalaList/KoalaTableDetail.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { TableRow , TableCell, Button } from '@material-ui/core';
 import {connect} from 'react-redux';
+import swal from 'sweetalert';
 
 class KoalaTableDetail extends Component {
 
@@ -10,20 +11,25 @@ class KoalaTableDetail extends Component {
     }
 
     deleteKoala = () => {
-        this.props.dispatch({ type: 'DELETE_KOALA', payload: this.props.koala })
+        this.props.dispatch({ type: 'DELETE_KOALA', payload: this.props.koala });
+        swal(`bye bye, ${this.props.koala.name}`);
     }
     render() {
+        let transfer;
+        if (this.props.koala.ready_to_transfer === true){
+            transfer = 'Y';
+        } else if (this.props.koala.ready_to_transfer === false){
+            transfer = <Button onClick={this.transferBtn}>Ready to Transfer</Button>;
+        }
         return (
             
                  <TableRow key={this.props.koala.i}>
                         <TableCell>{this.props.koala.name}</TableCell>
                         <TableCell>{this.props.koala.gender}</TableCell>
                         <TableCell>{this.props.koala.age}</TableCell>
-                        <TableCell>{this.props.koala.ready_to_transfer}</TableCell>
-                        <TableCell><Button onClick={this.transferBtn}>Ready to Transfer</Button></TableCell>
+                        <TableCell>{transfer}</TableCell>
                         <TableCell>{this.props.koala.notes}</TableCell>
                         <TableCell><Button onClick={this.deleteKoala}>Delete</Button></TableCell>
-                        
                     </TableRow>
            
         );
