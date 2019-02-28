b8c2b52db5afcd71eb12fe96f7b7617546f68b6d
mes32 <stockman.mike@gmail.com>
2019-02-28 13:42:12 -0600

    KoalaForm set state and handle changes.

--- src/components/KoalaForm/KoalaForm.js
+++ src/components/KoalaForm/KoalaForm.js
@@ -2,14 +2,43 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 class KoalaForm extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            name: '',
+            gender: '',
+            age: '',
+            ready_to_transfer: '',
+            notes: ''
+        };
+    }
 
-    // constructor();
+    // Runs when any form inputs are changed
+    handleChange = (event) => {
+        this.setState({
+            ...this.state,
+            [event.target.name]: event.target.value
+        });
+    }
 
+    // Runs when the form's submit 
+    handleSubmit = (event) => {
+        event.preventDefault();
+        console.log('in submit');
+        console.log(this.state);
+    }
+
+    // Display this component on the DOM
     render() {
         return (
-            <div>
-                <p>[ KoalaForm ]</p>
-            </div>
+            <form onSubmit={this.handleSubmit}>
+                <input onChange={this.handleChange} type="text" name="name" placeholder="Koala Name" />
+                <input onChange={this.handleChange} type="text" name="gender" placeholder="Sex" />
+                <input onChange={this.handleChange} type="number" name="age" placeholder="Age (years)" />
+                <input onChange={this.handleChange} type="text" name="ready_to_transfer" placeholder="Ready to Transfer" />
+                <input onChange={this.handleChange} type="text" name="notes" placeholder="Notes" />
+                <input type="submit" value="Submit" />
+            </form>
         );
     }
 }
